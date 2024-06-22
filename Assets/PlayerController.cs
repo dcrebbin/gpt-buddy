4c38cd6f3c63d5fc62ae3aeb8c703740d760625f
Devon Crebbin <devon@artvuu.group>
2024-06-23 04:28:36 +1000

    updated oai completion debug testing

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,37 @@ using Meta.WitAi.Events.UnityEventListeners;
 using Meta.Voice.Samples.Dictation;
 using Meta.WitAi.Events;
 using Meta.WitAi.Dictation;
+using TMPro;
+using UnityEngine.Networking;
+using Oculus.Platform;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+class OpenAIResponse
+{
+    public string id { get; set; }
+
+    public string created { get; set; }
+
+    public string model { get; set; }
+
+    public List<Choice> choices { get; set; }
+}
+
+class Choice
+{
+    public Message2 message { get; set; }
+    public string index { get; set; }
+    public string logprobs { get; set; }
+    public string finish_reason { get; set; }
+}
+
+class Message2
+{
+    public string role { get; set; }
+    public string content { get; set; }
+}
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,6 +46,14 @@ public class PlayerController : MonoBehaviour
 
     public DictationActivation dictationActivation;
     public AppDictationExperience appDictationExperience;
+
+    public TMP_Text text;
+
+    List<string> messages = new List<string>()
+        {
+            "{\"role\": \"system\", \"content\": \"You are a helpful assistant.\"}",
+        };
+
     private void Awake()
     {
         if (!witDictation) witDictation = FindObjectOfType<DictationService>();
@@ -27,6 +66,48 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("Transcript: " + transcript);
         // Update the transcript text
+        StartCoroutine(ChatCompletionRequest(transcript));
+    }
+
+
+    IEnumerator ChatCompletionRequest(string transcript)
+    {
+        var completionsEndpoint = "https://api.openai.com/v1/chat/completions";
+
+        messages.Add(",{\"role\": \"user\", \"content\": \"" + transcript + "\"}");
+        var json = new StringBuilder();
+        json.AppendLine("{");
+        json.AppendLine("\"model\": \"gpt-3.5-turbo-16k\",");
+        json.AppendLine("\"messages\": [");
+        foreach (var message in messages)
+        {
+            json.AppendLine(message);
+        }
+        json.AppendLine("]");
+        json.AppendLine("}");
+        Debug.Log(json.ToString());
+        var authToken = "";
+
+        using (UnityWebRequest www = UnityWebRequest.Post(completionsEndpoint, json.ToString(), "application/json"))
+        {
+            www.SetRequestHeader("Authorization", "Bearer " + authToken);
+            www.SetRequestHeader("Content-Type", "application/json");
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                var stringResponse = www.downloadHandler.text;
+                var response = Newtonsoft.Json.JsonConvert.DeserializeObject<OpenAIResponse>(stringResponse);
+                Debug.Log(response.created);
+                Debug.Log(response.choices[0].message.content);
+                text.text += "\n" + "Response: " + response.choices[0].message.content;
+                messages.Add(",{\"role\": \"assistant\", \"content\": \"" + response.choices[0].message.content + "\"}");
+            }
+        }
     }
 
     public void GotPartialTranscript(string transcript)
